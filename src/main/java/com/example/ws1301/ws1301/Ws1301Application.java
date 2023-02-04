8fdbf04305b4eab2a4e492c40ad8307ba2791580
Nilesh <nil.y.jadhav@gmail.com>
2023-02-04 14:41:58 +0800

    message

---
+++ src/main/java/com/example/ws1301/ws1301/Ws1301Application.java
@@ -0,0 +1,13 @@
+package com.example.ws1301.ws1301;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws1301Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws1301Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ws1301/ws1301/Ws1301ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ws1301.ws1301;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws1301ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
